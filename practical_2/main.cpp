91b3636564eb685d553c7a39449c0950f7ad9b32
Svetlozar Georgiev <sfe7lio@gmail.com>
2018-01-18 17:12:32 +0000

    added invaders and sprites for practical 2

---
+++ practical_2/main.cpp
@@ -0,0 +1,78 @@
+#include <SFML/Graphics.hpp>
+#include <iostream>
+#include "ship.h"
+#include "game.h"
+
+using namespace sf;
+using namespace std;
+
+Texture spritesheet;
+Sprite invader;
+vector<Ship *> ships;
+
+void Load() {
+	if (!spritesheet.loadFromFile("res/img/invaders_sheet.png")) {
+		cerr << "Failed to load spritesheet!" << endl;
+	}
+	
+	float x = 16.0f;
+	float y = 16.0f;
+
+	for (int i = 0; i < invaders_rows; ++i) {
+		auto rect = new IntRect(0, 0, 32, 32);
+		for (int j = 0; j < invaders_columns; ++j) {
+			auto pos = Vector2f{ x, y };
+			Invader *inv = new Invader(*rect, pos);
+			ships.push_back(inv);
+			x += 36.0f;
+		}
+		y += 36.0f;
+		x = 16.0f;
+	}
+}
+
+void Update(RenderWindow &window) {
+	// recalculate delta time
+	static Clock clock;
+	float dt = clock.restart().asSeconds();
+	//check and consume events
+	Event event;
+	while (window.pollEvent(event)) {
+		if (event.type == Event::Closed) {
+			window.close();
+			return;
+		}
+	}
+
+	//close the window when escape is pressed
+	if (Keyboard::isKeyPressed(Keyboard::Escape)) {
+		window.close();
+	}
+
+	//update ships
+	for (auto &s : ships) {
+		s->Update(dt);
+		cout << "Updating ships..." << endl;
+	}
+}
+
+void Render(RenderWindow &window) {
+	for (const auto s : ships) {
+		window.draw(*s);
+	}
+}
+
+int main() {
+	RenderWindow window(VideoMode(gameWidth, gameHeight), "SPACE INVADERS");
+
+	Load();
+
+	while (window.isOpen()) {
+		window.clear();
+		Update(window);
+		Render(window);
+		window.display();
+	}
+
+    return 0;
+  }
